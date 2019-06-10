5ff342ead5fd8c1c30fbe211311d12cc31bd7d06
lzq <1123816920@qq.com>
2019-06-10 15:11:43 +0800

    分支代码提交测试

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
+//测试
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
